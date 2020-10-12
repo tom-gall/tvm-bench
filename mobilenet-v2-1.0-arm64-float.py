a96a84cd5b5aeb464671cce85bb8a3352a0c35db
Tom Gall <tom.gall@linaro.org>
2020-10-12 19:00:44 +0000

    Combine and move to the new style with command line params and download the model
    Signed-off-by: Tom Gall <tom.gall@linaro.org>

--- mobilenet-v2-1.0-arm64-float.py
+++ mobilenet-v2-1.0-arm64-float.py
@@ -10,17 +10,19 @@ from tvm.runtime import vm as vm_rt
 from tvm.relay import testing
 from tvm.relay import vm
 from tvm.contrib.download import download_testdata
-from util import load_test_image
+from util import load_test_image, download_model_zoo,parse_options
+import sys
 
-model_url = "http://download.tensorflow.org/models/mobilenet_v1_2018_08_02/mobilenet_v1_1.0_224.tgz"
+argv=sys.argv[1:]
 
-# Download model tar file and extract it to get mobilenet_v1_1.0_224.tflite
-#model_path = download_testdata(model_url, "mobilenet_v1_1.0_224.tgz", module=['tf', 'official'])
-#model_dir = os.path.dirname(model_path)
-#extract(model_path)
-model_dir ="./mobilenet_v2_1.0_224"
-# Now we can open mobilenet_v1_1.0_224.tflite
-tflite_model_file = os.path.join(model_dir, "mobilenet_v2_1.0_224.tflite")
+device = parse_options(argv)
+
+model_dir ="/mobilenet_v2_1.0_224/"
+model_name = "mobilenet_v2_1.0_224.tflite"
+
+model_dir = download_model_zoo(model_dir, model_name)
+
+tflite_model_file = os.path.join(model_dir, model_name)
 tflite_model_buf = open(tflite_model_file, "rb").read()
 
 # Get TFLite model from buffer
@@ -36,16 +38,19 @@ image_data = load_test_image(dtype)
 
 input_tensor = "input"
 input_shape = (1, 224, 224, 3)
-input_dtype = "float32"
+input_dtype = dtype
 
 # Parse TFLite model and convert it to a Relay module
 mod, params = relay.frontend.from_tflite(tflite_model,
                                          shape_dict={input_tensor: input_shape},
                                          dtype_dict={input_tensor: input_dtype})
 
-# Build the module against ARM
-target = "llvm -device=arm_cpu -mtriple=aarch64-unknown-linux-gnu -mattr=+neon"
-tvm_targets = tvm.target.create(target)
+if device in ("llvm"):
+    target = "llvm -mcpu=thunderxt88 -mtriple=aarch64-unknown-linux-gnu -mattr=+neon,+crc,+lse"
+else:
+    target = "llvm -device=arm_cpu -mcpu=thunderxt88 -mtriple=aarch64-unknown-linux-gnu -mattr=+neon,+crc,+lse"
+
+tvm_targets = tvm.target.Target(target)
 cpu_target = "llvm"
 target_host=cpu_target
 
@@ -70,31 +75,5 @@ module.set_input(**params)
 
 ftimer = module.module.time_evaluator("run", ctx, number=1, repeat=10)
 prof_res = np.array(ftimer().results) * 1000  # multiply 1000 for converting to millisecond
-print("arm_cpu %-20s %-19s (%s)" % ("mobilenet_v2_1.0_224_float", "%.2f ms" % np.mean(prof_res), "%.2f ms" % np.std(prof_res)))
+print("%-20s %-7s %-19s (%s)" % (model_name, device, "%.2f ms" % np.mean(prof_res), "%.2f ms" % np.std(prof_res)))
 
-# Run
-#module.run()
-
-# Get output
-#tvm_output = module.get_output(0).asnumpy()
-
-# Load label file
-#label_file_url = ''.join(['https://raw.githubusercontent.com/',
-#                          'tensorflow/tensorflow/master/tensorflow/lite/java/demo/',
-#                          'app/src/main/assets/',
-#                          'labels_mobilenet_quant_v1_224.txt'])
-#label_file = "labels_mobilenet_quant_v1_224.txt"
-#label_path = download_testdata(label_file_url, label_file, module='data')
-
-# List of 1001 classes
-#with open(label_path) as f:
-#    labels = f.readlines()
-
-# Convert result to 1D data
-#predictions = np.squeeze(tvm_output)
-
-# Get top 1 prediction
-#prediction = np.argmax(predictions)
-
-# Convert id to class name and show the result
-#print("The image prediction result is: id " + str(prediction) + " name: " + labels[prediction])
