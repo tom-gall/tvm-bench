9210aec224df1fafe4e189ad3597681e55734bc9
Tom Gall <tom.gall@linaro.org>
2020-10-22 04:52:56 +0000

    Move over to new style that support both 64 and 32 bit
    Signed-off-by: Tom Gall <tom.gall@linaro.org>

--- mobilenet-v1.0.5-acl-float.py
+++ mobilenet-v1.0.5-acl-float.py
@@ -11,7 +11,7 @@ from tvm.relay import testing
 from tvm.relay import vm
 from tvm.relay.op.contrib import arm_compute_lib
 from tvm.contrib.download import download_testdata
-from util import load_test_image,build_module, update_lib, get_cpu_op_count,download_model_zoo,parse_options
+from util import load_test_image,build_module, update_lib, get_cpu_op_count,download_model_zoo,parse_options, get_device_arch, get_device_attributes, get_device_type, get_tvm_target
 import sys
 
 argv=sys.argv[1:]
@@ -48,13 +48,9 @@ mod, params = relay.frontend.from_tflite(tflite_model,
                                          shape_dict={input_tensor: input_shape},
                                          dtype_dict={input_tensor: input_dtype})
 
-# Build the module for a Arm CPU
-if device in ("llvm"):
-    target = "llvm -mcpu=thunderxt88 -mtriple=aarch64-unknown-linux-gnu -mattr=+neon,+crc,+lse"
-else:
-    target = "llvm -device=arm_cpu -mcpu=thunderxt88 -mtriple=aarch64-unknown-linux-gnu -mattr=+neon,+crc,+lse"
+tvm_target = get_tvm_target(device, get_device_type(), get_device_arch(), get_device_attributes())
 
-tvm_targets = tvm.target.Target(target)
+tvm_targets = tvm.target.Target(tvm_target)
 cpu_target = "llvm"
 target_host=cpu_target
 
@@ -85,52 +81,5 @@ gen_module.run()
 ftimer = gen_module.module.time_evaluator("run", ctx, number=1, repeat=10)
 prof_res = np.array(ftimer().results) * 1000  # multiply 1000 for converting to millisecond
 print("acl %-20s %-7s %-19s (%s)" % (model_name, device, "%.2f ms" % np.mean(prof_res), "%.2f ms" % np.std(prof_res)))
+print(tvm_target)
 
-
-#with tvm.transform.PassContext(opt_level=3):
-#    graph_mod = relay.build(mod, tvm_targets, params=params,target_host=target_host)
-
-#lib = graph_mod.get_lib()
-#params = graph_mod.get_params()
-#graph = graph_mod.get_json()
-
-# Create a runtime executor module
-#module = graph_runtime.create(graph, lib, cpudevice)
-
-# Feed input data
-#module.set_input(input_tensor, tvm.nd.array(image_data))
-
-# Feed related params
-#module.set_input(**params)
-
-#ftimer = module.module.time_evaluator("run", ctx, number=1, repeat=10)
-#prof_res = np.array(ftimer().results) * 1000  # multiply 1000 for converting to millisecond
-#print("%-20s %-19s (%s)" % (model_name, "%.2f ms" % np.mean(prof_res), "%.2f ms" % np.std(prof_res)))
-
-# Run
-#module.set_input(input_tensor, tvm.nd.array(image_data))
-#module.set_input(**params)
-#module.run()
-
-# Get output
-#tvm_output = module.get_output(0).asnumpy()
-
-# Load label file
-#label_file_url = ''.join(['https://raw.githubusercontent.com/',
-#                          'tensorflow/tensorflow/master/tensorflow/lite/java/demo/',
-#                          'app/src/main/assets/',
-#                          'labels_mobilenet_quant_v1_224.txt'])
-#label_file = "labels_mobilenet_quant_v1_224.txt"
-#label_path = download_testdata(label_file_url, label_file, module='data')
-# List of 1001 classes
-#with open(label_path) as f:
-#    labels = f.readlines()
-
-# Convert result to 1D data
-#predictions = np.squeeze(tvm_output)
-
-# Get top 1 prediction
-#prediction = np.argmax(predictions)
-
-# Convert id to class name and show the result
-#print("The image prediction result is: id " + str(prediction) + " name: " + labels[prediction])
