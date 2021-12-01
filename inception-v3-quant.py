3cc9face05ff3dc7d83c10718534c6ea6f65e04d
Tom Gall <tom_gall@mac.com>
2021-12-01 10:48:59 -0600

    Pass in host via the Target constructor instead of the soon to be deprecated relay.build call
    Signed-off-by: Tom Gall <tom_gall@mac.com>

--- inception-v3-quant.py
+++ inception-v3-quant.py
@@ -54,14 +54,13 @@ if device=="llvm":
 
 tvm_target = get_tvm_target(device, get_device_type(), get_device_arch(), get_device_attributes())
 
-tvm_targets = tvm.target.Target(tvm_target)
 cpu_target = "llvm"
-target_host=cpu_target
+tvm_targets = tvm.target.Target(tvm_target, host=cpu_target)
 
 cpudevice = tvm.runtime.cpu()
 
 with tvm.transform.PassContext(opt_level=3):
-    graph_mod = relay.build(mod, tvm_targets, params=params,target_host=target_host)
+    graph_mod = relay.build(mod, tvm_targets, params=params)
 
 params = graph_mod.get_params()
 
